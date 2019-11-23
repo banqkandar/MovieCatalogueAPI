6978ec31255f22047d0926d1d7a17c4e7c4b3cea
Bangkandar <misimatupang@gmail.com>
2019-11-23 18:31:47 +0700

    buat tombol back dng taskstackbuilder

--- app/src/main/java/com/bangkandar/moviecatalogue/TvShowDetailActivity.java
+++ app/src/main/java/com/bangkandar/moviecatalogue/TvShowDetailActivity.java
@@ -1,12 +1,16 @@
 package com.bangkandar.moviecatalogue;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.bangkandar.moviecatalogue.model.TvShow;
+
+import java.util.Objects;
 
 
 public class TvShowDetailActivity extends AppCompatActivity {
